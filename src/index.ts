02e3cef2465f4261a9542a820b232d05c7b9d67b
Zike420 <78177106+Zike420@users.noreply.github.com>
2023-06-07 18:29:07 -0300

    Nave volando

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,24 @@ const app = new Application({
 
 //const myLoader = new Loader();
 Loader.shared.add({url:"Nave.png",name:"Nave"});
+Loader.shared.add({url:"fuego.png",name:"Fuego"});
 
 Loader.shared.onComplete.add(()=>{
+	
+	const Nave: Sprite = Sprite.from("Nave.png");
+	const Fuego: Sprite = Sprite.from("fuego.png");
+
+	//Tamaño
+	Nave.width=312;
+	Nave.height=312;
+
+	Fuego.width=100;
+	Fuego.height=100;
 
 	//const clampy: Sprite = Sprite.from("clampy.png");
-	const Nave: Sprite = Sprite.from("Nave.png");
-
-	console.log("1 2 3", Nave.width, Nave.height);
+	
+	//Consola
+	console.log("1 2 3", "Nave:",Nave.width, Nave.height, "Fuego:",Fuego.width, Fuego.height);
 
 	//clampy.anchor.set(0.5);
 	//Nave.anchor.set(0.5);
@@ -29,11 +40,17 @@ Loader.shared.onComplete.add(()=>{
 	//clampy.x = 300;
 	//clampy.y = 300;
 
-	Nave.x = 300;
-	Nave.y = 300;
+	//Posicion
+	Nave.x = 150;
+	Nave.y = 60;
 
+	Fuego.x = 355;
+	Fuego.y = 458;
+
+	Fuego.rotation = 3.14;
 	//app.stage.addChild(clampy);
 	app.stage.addChild(Nave);
+	app.stage.addChild(Fuego);
 
 })
 
