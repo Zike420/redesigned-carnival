6afaba58cc6223b6891edb1914c567cd8d69f1f5
Zike420 <78177106+Zike420@users.noreply.github.com>
2023-07-18 22:32:46 -0300

    Player listo

--- src/Containers/Player.ts
+++ src/Containers/Player.ts
@@ -1,8 +1,83 @@
-import { PhysicsContainer } from "./PhysicsContainer";
+import { AnimatedSprite, Graphics, Texture } from "pixi.js";
+import { PhysicsContainer } from "../PhysicsContainer";
+import { Keyboard } from "../UI/Keyboard";
 
 export class Player extends PhysicsContainer{
+    private fuegoAnimado: AnimatedSprite;
+
+    private static readonly GRAVITY = 100;
+    private static readonly MOVE_SPEED = 150;
+
+    public canJump = true;
 
     constructor(){
         super();
+
+        this.fuegoAnimado = new AnimatedSprite([
+            Texture.from("Fuego1"),
+            Texture.from("Fuego2"),
+            Texture.from("Fuego3"),
+            Texture.from("Fuego4"),
+            Texture.from("Fuego5"),
+            Texture.from("Fuego6")
+        ], true);
+        this.fuegoAnimado.play();
+        this.fuegoAnimado.anchor.set(0.5,1);
+        this.fuegoAnimado.animationSpeed = 0.12;
+        this.addChild(this.fuegoAnimado);
+
+        
+        const auzZERO = new Graphics();
+        auzZERO.beginFill(0xFF00FF);
+        auzZERO.drawCircle(0,0,10);
+        auzZERO.endFill();
+
+        this.addChild(this.fuegoAnimado);
+        this.addChild(auzZERO);
+
+        this.acceletation.y = Player.GRAVITY;
+
+        Keyboard.down.on("ArrowUp", this.jump, this);
+    }
+
+    public override destroy(options:any){
+        super.destroy(options);
+        Keyboard.down.off("ArrowUp",this.jump);
+    }
+
+    public override update(deltaMS : number){
+        super.update(deltaMS/1000);
+        this.fuegoAnimado.update(deltaMS / (1000/60));
+
+        if(Keyboard.state.get("ArrowRight")){
+            this.speed.x = Player.MOVE_SPEED;
+            this.fuegoAnimado.scale.x=1;
+        }else if(Keyboard.state.get("ArrowLeft")){
+            this.speed.x = -Player.MOVE_SPEED;
+            this.fuegoAnimado.scale.x=-1;
+        }else{
+            this.speed.x = 0;
+        }
+
+        /*if(Keyboard.state.get("ArrowUp")){
+            this.jump();
+        }*/
+
+        /* PARTE IMPORTANTE PARA LA NAVE
+        if(Keyboard.state.get("ArrowUp")){
+            this.speed.y = -Player.MOVE_SPEED;
+        }else if(Keyboard.state.get("ArrowDown")){
+            this.speed.y = Player.MOVE_SPEED;
+        }else{
+            this.speed.y = 0;
+        }
+        */
+    }
+
+    private jump(){
+        if(this.canJump){
+            this.canJump = false;
+            this.speed.y = -100;
+        }
     }
 }
--- src/Scene/TickerScene.ts
+++ src/Scene/TickerScene.ts
@@ -1,73 +1,46 @@
-import { AnimatedSprite, Container, Graphics, Texture } from "pixi.js";
+import {Container} from "pixi.js";
 import { HEIGHT, WIDTH } from "..";
 import { IUpdateable } from "../IUpdateable";
-import { PhysicsContainer } from "../Containers/PhysicsContainer";
+//import { PhysicsContainer } from "../Containers/PhysicsContainer";
+import { Player } from "../Containers/Player";
 
 export class TickerScene extends Container implements IUpdateable{
 
-    private fuegoAnimado: AnimatedSprite;
-    private physFuego: PhysicsContainer;
+    private playerFuego : Player;
 
     constructor(){
         super();
 
-        this.fuegoAnimado = new AnimatedSprite([
-            Texture.from("Fuego1"),
-            Texture.from("Fuego2"),
-            Texture.from("Fuego3"),
-            Texture.from("Fuego4"),
-            Texture.from("Fuego5"),
-            Texture.from("Fuego6")
-        ], true);
-        this.fuegoAnimado.play();
-        this.fuegoAnimado.anchor.set(0.5,1);
-        this.fuegoAnimado.animationSpeed = 0.12;
-        this.addChild(this.fuegoAnimado);
-
-        this.physFuego = new PhysicsContainer();
-        this.physFuego.speed.x = 50;
-        this.physFuego.speed.y = 0;
-        this.physFuego.acceletation.y = 50;
-        this.addChild(this.physFuego);
-
-        const auzZERO = new Graphics();
-        auzZERO.beginFill(0xFF00FF);
-        auzZERO.drawCircle(0,0,10);
-        auzZERO.endFill();
-
-        this.physFuego.addChild(this.fuegoAnimado);
-        this.physFuego.addChild(auzZERO);
+        this.playerFuego = new Player();
+        this.addChild(this.playerFuego);
     }
 
-    public update(deltaTime: number, deltaFrame: number): void {
+    public update(deltaTime: number, _deltaFrame: number): void {
         //update de la animacion
-        this.fuegoAnimado.update(deltaFrame);
+        this.playerFuego.update(deltaTime);
 
         //delta time en segundos
-        const dt = deltaTime / 1000;
-        this.physFuego.update(dt);
+        //const dt = deltaTime / 1000;
+       // this.physFuego.update(dt);
         
         //limite horizontal
-        if(this.physFuego.x > WIDTH){
+        if(this.playerFuego.x > WIDTH){
             //Limite derecha
-                this.physFuego.x = WIDTH;
-                this.physFuego.speed.x = Math.abs(this.physFuego.speed.x) * -1;
-                this.physFuego.scale.x = -1;
-
-                this.fuegoAnimado.tint = 0x5FE84A;
-        }else if(this.physFuego.x < 0){
+                this.playerFuego.x = WIDTH;
+                //this.playerFuego.speed.x = Math.abs(this.playerFuego.speed.x) * -1;
+                //this.playerFuego.scale.x = -1;
+        }else if(this.playerFuego.x < 0){
             //Limite izquierda
-            this.physFuego.x = 0;
-            this.physFuego.speed.x = Math.abs(this.physFuego.speed.x);
-            this.physFuego.scale.x = 1;
-
-            this.fuegoAnimado.tint = 0;
+            this.playerFuego.x = 0;
+            //this.playerFuego.speed.x = Math.abs(this.playerFuego.speed.x);
+            //this.playerFuego.scale.x = 1;
         }
 
         //limite vertical
-        if(this.physFuego.y > HEIGHT){
-            this.physFuego.y = HEIGHT;
-            this.physFuego.speed.y = -100 * Math.random();            
+        if(this.playerFuego.y > HEIGHT){
+            this.playerFuego.y = HEIGHT;
+            this.playerFuego.canJump = true;
+            //this.playerFuego.speed.y = -100 * Math.random();            
         }
     }
 
