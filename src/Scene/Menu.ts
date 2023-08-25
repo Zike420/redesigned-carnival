11029629cf57cfe29914ceeaea3d6324f763e7b6
Zike420 <78177106+Zike420@users.noreply.github.com>
2023-08-25 20:06:11 -0300

    Menu updateable

--- src/Scene/Menu.ts
+++ src/Scene/Menu.ts
@@ -1,7 +1,8 @@
-import { Container, Sprite, Texture } from "pixi.js";
+import { Container, Texture } from "pixi.js";
+import { IUpdateable } from "../IUpdateable";
 import { Button } from "../UI/Button";
 
-export class Menu extends Container{
+export class Menu extends Container implements IUpdateable{
 
     private play : Button;
     private img : Texture;
@@ -11,6 +12,9 @@ export class Menu extends Container{
         this.play = new Button(this.img, this.img, this.img);
         this.addChild(this.play);
 
+    }
+    update(deltaTime: number, deltaFrame?: number | undefined): void {
+        throw new Error("Method not implemented.");
     }
 
 
