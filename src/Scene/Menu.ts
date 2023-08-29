861838a510b0bd17f1fa67c6e1e713ae6e1372d6
Zike420 <78177106+Zike420@users.noreply.github.com>
2023-08-29 17:37:10 -0300

    Menu casi listo

--- src/Scene/Menu.ts
+++ src/Scene/Menu.ts
@@ -1,6 +1,7 @@
-import { Container, Texture } from "pixi.js";
+import { Container, TextStyle, Texture, Text, Sprite } from "pixi.js";
 import { HEIGHT, WIDTH } from "..";
 import { Button } from "../UI/Button";
+import { TickerScene } from './SFNV';
 
 export class Menu extends Container{
 
@@ -18,6 +19,51 @@ export class Menu extends Container{
         this.play.position.set(WIDTH/2, HEIGHT/2)
         this.addChild(this.play);
 
+        const textStyle1 = new TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 36,
+            fill: 'white',
+            stroke: '#000000',
+            strokeThickness: 4,
+        });
+
+        //FONDO
+        const background = Sprite.from('SFMap');
+        background.width = WIDTH;
+        background.height = HEIGHT;
+        this.addChild(background);
+
+        //TITULO
+        const titleText : Text = new Text('Mini Bus', textStyle1);
+        titleText.position.x = WIDTH / 2 - titleText.width / 2;
+        titleText.position.y = 50;
+        this.addChild(titleText);
+
+        //BOTONES
+        const startText = new Text('Iniciar Juego', textStyle1);
+        startText.position.x = WIDTH / 2 - startText.width / 2;
+        startText.position.y = 500;
+        this.addChild(startText);
+
+        const optionsText = new Text('Opciones', textStyle1);
+        optionsText.position.x = WIDTH / 2 - optionsText.width / 2;
+        optionsText.position.y = 600;
+        this.addChild(optionsText);
+
+        startText.interactive = true;
+        startText.on('pointerdown', () => {
+            // Aquí puedes agregar la lógica para iniciar el juego
+            console.log('Iniciar juego...');
+            this.startGame();
+        });
+
     }
 
+
+    startGame() {
+        // Cambiar a la escena de juego
+        const gameScene = new TickerScene();
+        this.removeChildren();
+        this.addChild(gameScene);
+    }
 }
