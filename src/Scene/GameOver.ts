09bda6649fcccacea8fdb464bb2ac56d31ad60c9
Zike420 <78177106+Zike420@users.noreply.github.com>
2023-09-01 19:30:42 -0300

    Vuelta completa
    Pasar de menu a nivel y luego a game over, para volver a menu

--- src/Scene/GameOver.ts
+++ src/Scene/GameOver.ts
@@ -1,5 +1,48 @@
-import { Container } from "pixi.js";
+import { Container, TextStyle,Text } from "pixi.js";
+import { WIDTH } from "..";
+import { Menu } from "./Menu";
 
 export class GameOver extends Container{
     
+    constructor(){
+        super();
+        const textStyle1 = new TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 36,
+            fill: 'white',
+            stroke: '#000000',
+            strokeThickness: 4,
+        });
+
+        //TITULO
+        const titleText : Text = new Text('Game Over', textStyle1);
+        titleText.position.x = WIDTH / 2 - titleText.width / 2;
+        titleText.position.y = 50;
+        this.addChild(titleText);
+
+        //BOTONES
+        const retryText = new Text('Intentar de nuevo', textStyle1);
+        retryText.position.x = WIDTH / 2 - retryText.width / 2;
+        retryText.position.y = 500;
+        this.addChild(retryText);
+
+        const backText = new Text('Volver', textStyle1);
+        backText.position.x = WIDTH / 2 - backText.width / 2;
+        backText.position.y = 600;
+        this.addChild(backText);
+
+        backText.interactive = true;
+        backText.on('pointerdown', () => {
+            
+            console.log('Ir al menu...');
+            this.GameOver();
+        });
+    }
+    
+    GameOver() {
+        // Cambiar a la escena de juego
+        const gameScene = new Menu();
+        this.removeChildren();
+        this.addChild(gameScene);
+    }
 }
--- src/Scene/SFNV.ts
+++ src/Scene/SFNV.ts
@@ -1,6 +1,7 @@
-import {Container, Sprite} from "pixi.js";
+import {Container, Sprite, Text} from "pixi.js";
 import { HEIGHT, WIDTH } from "..";
 import { IUpdateable } from "../IUpdateable";
+import { GameOver } from './GameOver';
 //import { PhysicsContainer } from "../Containers/PhysicsContainer";
 
 
@@ -8,9 +9,13 @@ export class TickerScene extends Container implements IUpdateable{
 
     private Fondo : Sprite;
 
+    private startTime = Date.now();
+    private timerInterval: string | number | NodeJS.Timeout | undefined;
+    private timerText : Text;
+
     constructor(){
         super();
-        
+
         this.Fondo = Sprite.from("SFMap");
 
         this.Fondo.scale.set(1.91,1.91);
@@ -23,26 +28,70 @@ export class TickerScene extends Container implements IUpdateable{
         this.Fondo.on("mouseup", this.onMouseUp, this);
 
         this.addChild(this.Fondo);
+
+        //////////////////////////////
+        this.timerText = new Text('Tiempo: 0', { fontSize: 24, fill: 0xffffff });
+        this.timerText.position.set(10, 10);
+        this.addChild(this.timerText);
+
+        // Botones para iniciar y detener el temporizador
+        const startButton = new Text('Iniciar', { fontSize: 18, fill: 0x00ff00 });
+        startButton.position.set(10, 50);
+        startButton.interactive = true;
+        startButton.buttonMode = true;
+        startButton.on('pointerdown', this.startTimer);
+        this.addChild(startButton);
+
+        const stopButton = new Text('Detener', { fontSize: 18, fill: 0xff0000 });
+        stopButton.position.set(100, 50);
+        stopButton.interactive = true;
+        stopButton.buttonMode = true;
+        stopButton.on('pointerdown', () => {
+            
+          console.log('Ir al menu...');
+          this.GameOver();
+        });
+        this.addChild(stopButton);
+        //////////////////////////////
     }
     
+  ///////////////////////////////////////////////
+  // Función para actualizar el temporizador
+  public updateTimer() {
+    const currentTime = Date.now();
+    const elapsedTime = Math.floor((currentTime - this.startTime) / 1000);
+    this.timerText.text = `Tiempo: ${elapsedTime} segundos`;
+  }
 
-    public update(deltaTime: number, _deltaFrame: number): void {
-        deltaTime;
-    }
+  // Iniciar el temporizador
+  public startTimer() {
+    this.startTime = Date.now();
+    this.timerInterval = setInterval(this.updateTimer, 1000);
+  }
 
-    private onMouseWheel(event: WheelEvent): void {
-      this.scale.x -= event.deltaY * 0.1;
-      this.scale.y -= event.deltaY * 0.1;
-      console.log("wheel", this);
-    }
+    // Detener el temporizador
+  public stopTimer() {
+    clearInterval(this.timerInterval);
+  }
+  //////////////////////////////////////////////
 
-    private onPointerDown():void {
-      this.scale.x += this.scale.x * 0.1;
-      this.scale.y += this.scale.y * 0.1;
-      console.log("point down", this);
-   }
+  public update(deltaTime: number, _deltaFrame: number): void {
+      deltaTime;
+  }
 
-   private onMouseUp():void {
+  private onMouseWheel(event: WheelEvent): void {
+    this.scale.x -= event.deltaY * 0.1;
+    this.scale.y -= event.deltaY * 0.1;
+    console.log("wheel", this);
+  }
+
+  private onPointerDown():void {
+    this.scale.x += this.scale.x * 0.1;
+    this.scale.y += this.scale.y * 0.1;
+    console.log("point down", this);
+  }
+
+  private onMouseUp():void {
     this.scale.x = 1;
     this.scale.y = 1;
     console.log("mouse up", this);
@@ -52,9 +101,15 @@ export class TickerScene extends Container implements IUpdateable{
       this.scale.y -= event.deltaY *0.001;  
     });
   
- }
+  }
+
+  GameOver() {
+    // Cambiar a la escena de juego
+    const gameOver = new GameOver();
+    this.removeChildren();
+    this.addChild(gameOver);
+  }
 
 
 } 
 
-    
